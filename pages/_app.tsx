b892d2f215eca03e22dbf4e685a545b0f4eea105
ando_ta <ando_ta@mti.co.jp>
2022-07-20 09:59:20 +0900

    Reduxの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import {store} from "/redux/Store";
+
+import {Provider} from "react-redux";
+
+// 全てのコンポーネントからstoreにアクセスできるように、Providerで包みます
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
---
+++ redux/NewsSlice.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+import { client } from "/libs/client";
+
+type DateType = {
+  category: object;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  id: string;
+  title: string;
+}[];
+
+type Props = {
+  news: DateType | [];
+  error: boolean;
+};
+
+export const getNews = createAsyncThunk("news/getNews", async () => {
+  const data = await client.get({ endpoint: "news" });
+  return data.contents;
+});
+
+const initialState: Props = {
+  news: [],
+  error: false,
+};
+
+const newsSlice = createSlice({
+  name: "news",
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(getNews.fulfilled, (state, action: PayloadAction<DateType>) => {
+        state.news = action.payload;
+      })
+      .addCase(getNews.rejected, state => {
+        state.error = true;
+      });
+  },
+});
+
+export default newsSlice.reducer;
